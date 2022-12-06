b4cb41c013f18fcb3097a280d05b634129087aeb
arizonv <a.vasgarridoe@gmail.com>
2022-12-06 20:48:40 +0100

    mode hub sound

--- registrApp/urls.py
+++ registrApp/urls.py
@@ -3,8 +3,7 @@ from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
